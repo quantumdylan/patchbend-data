661430ec4038f5ad8ad967442dc515b8654f39a6
Adam Wan <adamwan92@gmail.com>
2017-03-15 00:10:21 +0800

    init

---
+++ synths.js
@@ -0,0 +1,23 @@
+export const synths = [
+  {
+    name: 'Korg Minilogue'
+  },
+  {
+    name: 'Korg Monologue'
+  },
+  {
+    name: 'Korg ARP Odyssey'
+  },
+  {
+    name: 'Korg Volca Bass'
+  },
+  {
+    name: 'Korg Volca Keys'
+  },
+  {
+    name: 'Korg Volca FM'
+  },
+  {
+    name: 'Yamaha DX-7'
+  },
+];
