5c9d9676b71e82c2bfb32459ef86da3771479392
Adam Wan <adamwan92@gmail.com>
2017-04-25 22:16:42 +0800

    new synths

--- synths.js
+++ synths.js
@@ -1,5 +1,53 @@
 export const synths = [
   {
+    name: 'Access Virus TI2'
+  },
+  {
+    name: 'Arturia Matrixbrute'
+  },
+  {
+    name: 'Arturia Microbrute'
+  },
+  {
+    name: 'Arturia Minibrute'
+  },
+  {
+    name: 'Behringer DeepMind 6'
+  },
+  {
+    name: 'Behringer DeepMind 12'
+  },
+  {
+    name: 'DSI Mopho x4'
+  },
+  {
+    name: 'DSI OB6'
+  },
+  {
+    name: 'DSI Pro 2'
+  },
+  {
+    name: 'DSI Prophet 12'
+  },
+  {
+    name: 'DSI Prophet Rev2'
+  },
+  {
+    name: 'Sequential Prophet 6'
+  },
+  {
+    name: 'Elektron Analog Four'
+  },
+  {
+    name: 'Elektron Analog Keys'
+  },
+  {
+    name: 'Korg Electribe'
+  },
+  {
+    name: 'Korg MS-20'
+  },
+  {
     name: 'Korg Minilogue'
   },
   {
@@ -18,6 +66,159 @@ export const synths = [
     name: 'Korg Volca FM'
   },
   {
+    name: 'Korg Volca Kick'
+  },
+  {
+    name: 'Korg MicroKORG'
+  },
+  {
+    name: 'Korg MicroKORG XL+'
+  },
+  {
+    name: 'Modal 001'
+  },
+  {
+    name: 'Modal 008'
+  },
+  {
+    name: 'Moog Minimoog Modal D'
+  },
+  {
+    name: 'Moog Minimoog Voyager XL'
+  },
+  {
+    name: 'Moog Minitaur'
+  },
+  {
+    name: 'Moog Mother-32'
+  },
+  {
+    name: 'Moog Sub 37'
+  },
+  {
+    name: 'Moog Sub Phatty'
+  },
+  {
+    name: 'Nord Lead 4'
+  },
+  {
+    name: 'Nord Lead A1'
+  },
+  {
+    name: 'Nord Stage 2'
+  },
+  {
+    name: 'Nord Stage 3'
+  },
+  {
+    name: 'Novation Bass Station II'
+  },
+  {
+    name: 'Novation Circuit Mono Station'
+  },
+  {
+    name: 'Novation Circuit'
+  },
+  {
+    name: 'Novation MiniNova'
+  },
+  {
+    name: 'Novation Peak'
+  },
+  {
+    name: 'Novation UltraNova'
+  },
+  {
+    name: 'Pioneer DJ Toraiz AS-1'
+  },
+  {
+    name: 'Roland JP-08'
+  },
+  {
+    name: 'Roland JU-06'
+  },
+  {
+    name: 'Roland JX-03'
+  },
+  {
+    name: 'Roland GAIA SH-01'
+  },
+  {
+    name: 'Roland Integra-7'
+  },
+  {
+    name: 'Roland JD-XA'
+  },
+  {
+    name: 'Roland JD-Xi'
+  },
+  {
+    name: 'Roland JUNO DS61'
+  },
+  {
+    name: 'Roland JUNO DS88'
+  },
+  {
+    name: 'Roland Jupiter-80'
+  },
+  {
+    name: 'Roland System-1'
+  },
+  {
+    name: 'Roland System-8'
+  },
+  {
+    name: 'Roland TB-03'
+  },
+  {
+    name: 'Roland TB-3'
+  },
+  {
+    name: 'Teenage Engineering OP1'
+  },
+  {
+    name: 'Waldorf Blofeld'
+  },
+  {
+    name: 'Waldorf Rocket'
+  },
+  {
+    name: 'Waldorf Streichfett'
+  },
+  {
+    name: 'Yamaha MX49'
+  },
+  {
+    name: 'Yamaha MX88'
+  },
+  {
+    name: 'Yamaha Montage'
+  },
+  {
+    name: 'Yamaha Reface CS'
+  },
+  {
+    name: 'Yamaha Reface DX'
+  },
+  {
     name: 'Yamaha DX-7'
+  },
+  {
+    name: 'U-he Diva'
+  },
+  {
+    name: 'U-he Zebra 2'
+  },
+  {
+    name: 'U-he Hive'
+  },
+  {
+    name: 'Massive'
+  },
+  {
+    name: 'FM8'
+  },
+  {
+    name: 'Sylenth1'
   },
 ];
