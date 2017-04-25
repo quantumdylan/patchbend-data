8001d95acc04077bd2e476c112220d52441cf725
Adam Wan <adamwan92@gmail.com>
2017-04-25 22:25:03 +0800

    effects

--- effects.js
+++ effects.js
@@ -1,6 +1,60 @@
 export const effects = [
   {
+    name: 'Axe-FX'
+  },
+  {
+    name: 'Eventide H9'
+  },
+  {
+    name: 'Eventide Space'
+  },
+  {
+    name: 'Eventide TimeFactor'
+  },
+  {
+    name: 'Eventide ModFactor'
+  },
+  {
+    name: 'Eventide PitchFactor'
+  },
+  {
     name: 'Strymon Timeline'
+  },
+  {
+    name: 'Strymon Mobius'
+  },
+  {
+    name: 'Strymon BigSky'
+  },
+  {
+    name: 'Boss DD-500'
+  },
+  {
+    name: 'Boss ME-80'
+  },
+  {
+    name: 'Boss GT-1'
+  },
+  {
+    name: 'Boss GT-100'
+  },
+  {
+    name: 'Boss ME-25'
+  },
+  {
+    name: 'Line 6 Helix'
+  },
+  {
+    name: 'Line 6 Firehawk'
+  },
+  {
+    name: 'Line 6 M9'
+  },
+  {
+    name: 'Line 6 M13'
+  },
+  {
+    name: 'Line 6 POD HD500X'
   },
 
 ];
